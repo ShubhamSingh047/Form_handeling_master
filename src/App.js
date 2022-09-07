4d8df4a6afa9d0d75497af4a5d57a027cf0ee0ed
Shivam <thisisshivam06@gmail.com>
2022-09-08 02:34:28 +0530

    Upload Image functionality done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import FormExampleForm from './Components/Form'
-import Single_Handeling from './Components/Single_Handeling';
+import "./App.css";
+import FormExampleForm from "./Components/Form";
+import Single_Handeling from "./Components/Single_Handeling";
 
 function App() {
   return (
     <div className="App">
-      {/* <FormExampleForm /> */}
-      <Single_Handeling />
+      <FormExampleForm />
+      {/* <Single_Handeling /> */}
     </div>
   );
 }
